d55fa0047f3d3ab427458490e35b644761baf214
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-04-03 16:40:46 +0530

    Update varaibles.tf

--- terraform/primary-region/varaibles.tf
+++ terraform/primary-region/varaibles.tf
@@ -1,1 +1,61 @@
+variable "project_name" {
+  description = "Name of the project"
+  type        = string
+  default     = "dr-demo"
+}
+
+variable "primary_region" {
+  description = "Primary AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "secondary_region" {
+  description = "Secondary AWS region for DR"
+  type        = string
+  default     = "us-west-2"
+}
+
+variable "domain_name" {
+  description = "Domain name for the application"
+  type        = string
+  default     = "example.com"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version for EKS cluster"
+  type        = string
+  default     = "1.28"
+}
+
+variable "node_instance_type" {
+  description = "EC2 instance type for EKS nodes"
+  type        = string
+  default     = "t3.medium"
+}
+
+variable "node_desired_size" {
+  description = "Desired number of nodes in the EKS node group"
+  type        = number
+  default     = 3
+}
+
+variable "node_max_size" {
+  description = "Maximum number of nodes in the EKS node group"
+  type        = number
+  default     = 3
+}
+
+variable "node_min_size" {
+  description = "Minimum number of nodes in the EKS node group"
+  type        = number
+  default     = 1
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "production"
+}
+
 
