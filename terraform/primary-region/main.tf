78792295b25759b6f8c38e487a3438a1aac485d7
rahulb3141 <vijaya-rahul.bollu@capgemini.com>
2026-04-04 11:12:55 +0530

    Update main.tf

--- terraform/primary-region/main.tf
+++ terraform/primary-region/main.tf
@@ -130,90 +130,90 @@ resource "aws_s3_bucket_replication_configuration" "replication" {
   depends_on = [aws_s3_bucket_versioning.primary_backups]
 }
 
-# Route 53 Hosted Zone
-resource "aws_route53_zone" "main" {
-  name = var.domain_name
+# # Route 53 Hosted Zone
+# resource "aws_route53_zone" "main" {
+#   name = var.domain_name
 
-  # Associate with existing VPC if specified
-  dynamic "vpc" {
-    for_each = var.associate_with_vpc ? [1] : []
-    content {
-      vpc_id = data.aws_vpc.existing.id
-    }
-  }
+#   # Associate with existing VPC if specified
+#   dynamic "vpc" {
+#     for_each = var.associate_with_vpc ? [1] : []
+#     content {
+#       vpc_id = data.aws_vpc.existing.id
+#     }
+#   }
 
-  tags = {
-    Name    = "${var.project_name}-hosted-zone"
-    Project = var.project_name
-    VPC     = data.aws_vpc.existing.id
-  }
-}
+#   tags = {
+#     Name    = "${var.project_name}-hosted-zone"
+#     Project = var.project_name
+#     VPC     = data.aws_vpc.existing.id
+#   }
+# }
 
-# Route 53 Health Check for Primary Region
-resource "aws_route53_health_check" "primary" {
-  fqdn                            = var.primary_app_endpoint
-  port                            = var.health_check_port
-  type                            = var.health_check_protocol
-  resource_path                   = var.health_check_path
-  failure_threshold               = 3
-  request_interval                = 30
+# # Route 53 Health Check for Primary Region
+# resource "aws_route53_health_check" "primary" {
+#   fqdn                            = var.primary_app_endpoint
+#   port                            = var.health_check_port
+#   type                            = var.health_check_protocol
+#   resource_path                   = var.health_check_path
+#   failure_threshold               = 3
+#   request_interval                = 30
 
-  tags = {
-    Name    = "${var.project_name}-primary-health-check"
-    Project = var.project_name
-  }
-}
+#   tags = {
+#     Name    = "${var.project_name}-primary-health-check"
+#     Project = var.project_name
+#   }
+# }
 
-# Route 53 Health Check for Secondary Region
-resource "aws_route53_health_check" "secondary" {
-  fqdn                            = var.secondary_app_endpoint
-  port                            = var.health_check_port
-  type                            = var.health_check_protocol
-  resource_path                   = var.health_check_path
-  failure_threshold               = 3
-  request_interval                = 30
+# # Route 53 Health Check for Secondary Region
+# resource "aws_route53_health_check" "secondary" {
+#   fqdn                            = var.secondary_app_endpoint
+#   port                            = var.health_check_port
+#   type                            = var.health_check_protocol
+#   resource_path                   = var.health_check_path
+#   failure_threshold               = 3
+#   request_interval                = 30
 
-  tags = {
-    Name    = "${var.project_name}-secondary-health-check"
-    Project = var.project_name
-  }
-}
+#   tags = {
+#     Name    = "${var.project_name}-secondary-health-check"
+#     Project = var.project_name
+#   }
+# }
 
-# Route 53 DNS Records for Failover - Primary
-resource "aws_route53_record" "primary" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "app.${var.domain_name}"
-  type    = "CNAME"
-  ttl     = 60
+# # Route 53 DNS Records for Failover - Primary
+# resource "aws_route53_record" "primary" {
+#   zone_id = aws_route53_zone.main.zone_id
+#   name    = "app.${var.domain_name}"
+#   type    = "CNAME"
+#   ttl     = 60
 
-  failover_routing_policy {
-    type = "PRIMARY"
-  }
+#   failover_routing_policy {
+#     type = "PRIMARY"
+#   }
 
-  health_check_id = aws_route53_health_check.primary.id
-  set_identifier  = "primary"
-  records         = [var.primary_app_endpoint]
-}
+#   health_check_id = aws_route53_health_check.primary.id
+#   set_identifier  = "primary"
+#   records         = [var.primary_app_endpoint]
+# }
 
-# Route 53 DNS Records for Failover - Secondary
-resource "aws_route53_record" "secondary" {
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "app.${var.domain_name}"
-  type    = "CNAME"
-  ttl     = 60
+# # Route 53 DNS Records for Failover - Secondary
+# resource "aws_route53_record" "secondary" {
+#   zone_id = aws_route53_zone.main.zone_id
+#   name    = "app.${var.domain_name}"
+#   type    = "CNAME"
+#   ttl     = 60
 
-  failover_routing_policy {
-    type = "SECONDARY"
-  }
+#   failover_routing_policy {
+#     type = "SECONDARY"
+#   }
 
-  health_check_id = aws_route53_health_check.secondary.id
-  set_identifier  = "secondary"
-  records         = [var.secondary_app_endpoint]
-}
+#   health_check_id = aws_route53_health_check.secondary.id
+#   set_identifier  = "secondary"
+#   records         = [var.secondary_app_endpoint]
+# }
 
-# Random string for unique bucket names
-resource "random_string" "bucket_suffix" {
-  length  = 8
-  special = false
-  upper   = false
-}
+# # Random string for unique bucket names
+# resource "random_string" "bucket_suffix" {
+#   length  = 8
+#   special = false
+#   upper   = false
+# }
